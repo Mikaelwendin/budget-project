22470b1de4167a5ae046f10560d4fb94ebdb80f8
Mikaelwendin <mikael.wendin83@gmail.com>
2024-05-31 14:38:44 +0200

    basic registration to db

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,50 @@
+"use client"
 import React from 'react'
 
 const Register = () => {
+
+
+const handleSubmit = async(e: any) => {
+
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    console.log(email, password)
+
+    try {
+        const res = await fetch("/api/register" , {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                email, password
+            })
+        })
+        if (res.status === 400) {
+            console.log("email already registred")
+        }
+        if (res.status === 200) {
+            console.log("Success")
+        }
+        
+    } catch (error) {
+        console.log("Error!!!")
+    }
+}
+
+
   return (
     <div>
-      Register
+    <div><h1>Register</h1>
+    <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='email' required></input>
+        <input type='password' placeholder='password' required></input>
+        <button type='submit'>Go</button>
+    </form>
+    </div>
     </div>
   )
 }
